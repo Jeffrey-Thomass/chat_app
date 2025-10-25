35ceb82e60b52f44306d441074accd6cb707a80b
Jeffrey Thomas <jeffreythomas10566x@gmail.com>
2025-10-25 12:23:53 +0530

    ready for deployment

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -12,6 +12,6 @@ router.get("/login" , (req,res) => {
 
 router.get("/logout" , (req,res) => {
     res.send("logout endpoint")
-})
+}) 
 
 export default router
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,15 +2,28 @@ import express from "express";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.route.js";
 import messageRouter from "./routes/message.route.js";
+import path from "path";
 
 
 dotenv.config();
 
 const app = express();
+
+const __dirname = path.resolve();  // returns the CWD 
+console.log(__dirname)
+
 const port = process.env.PORT
 
 app.use("/api/auth", authRouter)
 app.use("/api/messages", messageRouter)
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get(/.*/, (_, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    })
+}
 
 app.listen(port, () => {
     console.log("Server is running on port " + port);
