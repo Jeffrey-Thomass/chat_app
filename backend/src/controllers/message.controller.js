8f3c00b63d71c0861765c05bc81127df280a5a8f
Jeffrey Thomas <jeffreythomas10566x@gmail.com>
2026-01-12 18:49:37 +0530

    corrected zustand import

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -30,7 +30,7 @@ export const getMessagesByUserId = async (req , res) => {
                     receiverId : myId
                 }
             ]
-        })
+        }).sort({createdAt : -1})                   
         res.status(200).json(message);
     }
     catch(err){
@@ -45,14 +45,14 @@ export const sendMessage = async (req, res) => {
         const senderId = req.user._id;
         if (!text && !image) {
             return res.status(400).json({ message: "Text or image is required." });
-          }
-          if (senderId.equals(receiverId)) {
+        }
+        if (senderId.equals(receiverId)) {
             return res.status(400).json({ message: "Cannot send messages to yourself." });
-          }
-          const receiverExists = await User.exists({ _id: receiverId });
-          if (!receiverExists) {
+        }
+        const receiverExists = await User.exists({ _id: receiverId });
+        if (!receiverExists) {
             return res.status(404).json({ message: "Receiver not found." });
-          }
+        } 
         let imageUrl;
         if ( image ){
             const uploadResponse = await cloudinary.uploader.upload(image);
